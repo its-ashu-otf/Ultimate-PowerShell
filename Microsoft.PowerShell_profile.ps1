91784e9a431a11addebf8d18c270d3155cb52bd0
Ashutosh <ashutoshtips@gmail.com>
2024-06-07 21:00:19 +0530

    Update Microsoft.PowerShell_profile.ps1
    - Added HasteBin Function.
    - Fixed some backgroud issues.
    - Added 'gc' gitclone alias.
    - More That i probably forgot...

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -102,14 +102,39 @@ function Edit-Profile {
 function touch($file) { "" | Out-File $file -Encoding ASCII }
 function ff($name) {
     Get-ChildItem -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | ForEach-Object {
-        Write-Output "$($_.directory)\$($_)"
+        Write-Output "$($_.FullName)"
     }
 }
 
 # Network Utilities
 function Get-PubIP { (Invoke-WebRequest http://ifconfig.me/ip).Content }
 
+function flushdns {
+	Clear-DnsClientCache
+	Write-Host "DNS has been flushed"
+}
+
 # System Utilities
+
+# sudo funcnalitiy
+function admin {
+    if ($args.Count -gt 0) {
+        $argList = "& '$args'"
+        Start-Process wt -Verb runAs -ArgumentList "pwsh.exe -NoExit -Command $argList"
+    } else {
+        Start-Process wt -Verb runAs
+    }
+}
+
+# Set UNIX-like aliases for the admin command, so sudo <command> will run the command with elevated rights.
+Set-Alias -Name su -Value admin
+Set-Alias -Name sudo -Value admin
+
+# Open WinUtil
+function winutil {
+	iwr -useb https://christitus.com/win | iex
+}
+
 function uptime {
     if ($PSVersionTable.PSVersion.Major -eq 5) {
         Get-WmiObject win32_operatingsystem | Select-Object @{Name='LastBootUpTime'; Expression={$_.ConverttoDateTime($_.lastbootuptime)}} | Format-Table -HideTableHeaders
@@ -190,8 +215,8 @@ function head {
 }
 
 function tail {
-  param($Path, $n = 10)
-  Get-Content $Path -Tail $n
+  param($Path, $n = 10, [switch]$f = $false)
+  Get-Content $Path -Tail $n -Wait:$f
 }
 
 # Quick File Creation
@@ -221,6 +246,8 @@ function ll { Get-ChildItem -Path . -Force -Hidden | Format-Table -AutoSize }
 function gs { git status }
 
 function ga { git add . }
+
+function gcl { git clone "$args" }
 
 function gc { param($m) git commit -m "$m" }
 
@@ -255,6 +282,77 @@ Set-PSReadLineOption -Colors @{
     Parameter = 'Green'
     String = 'DarkCyan'
 }
+
+$PSROptions = @{
+    ContinuationPrompt = '  '
+    Colors             = @{
+    Parameter          = $PSStyle.Foreground.Magenta
+    Selection          = $PSStyle.Background.Black
+    InLinePrediction   = $PSStyle.Foreground.BrightYellow + $PSStyle.Background.BrightBlack
+    }
+}
+Set-PSReadLineOption @PSROptions
+Set-PSReadLineKeyHandler -Chord 'Ctrl+f' -Function ForwardWord
+Set-PSReadLineKeyHandler -Chord 'Enter' -Function ValidateAndAcceptLine
+
+$scriptblock = {
+    param($wordToComplete, $commandAst, $cursorPosition)
+    dotnet complete --position $cursorPosition $commandAst.ToString() |
+        ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock $scriptblock
+
+
+# Help Function
+function Show-Help {
+    @"
+PowerShell Profile Help
+=======================
+Update-Profile - Checks for profile updates from a remote repository and updates if necessary.
+Update-PowerShell - Checks for the latest PowerShell release and updates if a new version is available.
+Edit-Profile - Opens the current user's profile for editing using the configured editor.
+touch <file> - Creates a new empty file.
+ff <name> - Finds files recursively with the specified name.
+Get-PubIP - Retrieves the public IP address of the machine.
+winutil - Runs the WinUtil script from Chris Titus Tech.
+uptime - Displays the system uptime.
+reload-profile - Reloads the current user's PowerShell profile.
+unzip <file> - Extracts a zip file to the current directory.
+hb <file> - Uploads the specified file's content to a hastebin-like service and returns the URL.
+grep <regex> [dir] - Searches for a regex pattern in files within the specified directory or from the pipeline input.
+df - Displays information about volumes.
+sed <file> <find> <replace> - Replaces text in a file.
+which <name> - Shows the path of the command.
+export <name> <value> - Sets an environment variable.
+pkill <name> - Kills processes by name.
+pgrep <name> - Lists processes by name.
+head <path> [n] - Displays the first n lines of a file (default 10).
+tail <path> [n] - Displays the last n lines of a file (default 10).
+nf <name> - Creates a new file with the specified name.
+mkcd <dir> - Creates and changes to a new directory.
+docs - Changes the current directory to the user's Documents folder.
+dtop - Changes the current directory to the user's Desktop folder.
+ep - Opens the profile for editing.
+k9 <name> - Kills a process by name.
+la - Lists all files in the current directory with detailed formatting.
+ll - Lists all files, including hidden, in the current directory with detailed formatting.
+gs - Shortcut for 'git status'.
+ga - Shortcut for 'git add .'.
+gc <message> - Shortcut for 'git commit -m'.
+gp - Shortcut for 'git push'.
+g - Changes to the GitHub directory.
+gcom <message> - Adds all changes and commits with the specified message.
+lazyg <message> - Adds all changes, commits with the specified message, and pushes to the remote repository.
+sysinfo - Displays detailed system information.
+flushdns - Clears the DNS cache.
+cpy <text> - Copies the specified text to the clipboard.
+pst - Retrieves text from the clipboard.
+Use 'Show-Help' to display this help message.
+"@
+}
+Write-Host "Use 'Show-Help' to display help"
 
 ## Final Line to set prompt
 oh-my-posh init pwsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/cobalt2.omp.json | Invoke-Expression
