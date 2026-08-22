db857296996a56c2e8144c0273e8f158267ee68e
Ashutosh Gupta <itsashuotf@gmail.com>
2026-08-22 13:41:29 +0530

    Add version display to PowerShell setup script

--- setup.ps1
+++ setup.ps1
@@ -10,6 +10,7 @@ $banner = @"
 Write-Host $banner -ForegroundColor Blue
 Write-Host "Author: its-ashu-otf" -ForegroundColor Cyan
 Write-Host "Welcome to the Ultimate PowerShell Setup Script!" -ForegroundColor Green
+Write-Host "Version: 3.0" -ForegroundColor Green
 Write-Host ""
 
 $profileSourceUri = 'https://github.com/its-ashu-otf/Ultimate-PowerShell/raw/main/Microsoft.PowerShell_profile.ps1'
